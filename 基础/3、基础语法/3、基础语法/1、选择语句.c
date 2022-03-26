7eb9070496cf456143f38cf518551e0a272a1f9d
cha-yubai <2946053797@qq.com>
2022-03-26 10:24:33 +0800

    基础篇-选择语句

---
+++ 基础/3、基础语法/3、基础语法/1、选择语句.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//选择语句if
+	//如果你好好学习，校招时拿好offer，走上人生巅峰
+	//如果你不学习，毕业等于失业，回家卖红薯
+//#include <stdio.h>
+//int main()
+//{
+//	int input = 0;
+//	printf("你会去敲代码吗？（选择1 or 0）:>");
+//	scanf("%d",&input);
+//	if (input == 1)
+//	{
+//		printf("坚持，你会有好offer！\n");
+//	}
+//	else
+//	{
+//		printf("放弃，回家卖红薯！\n");
+//	}
+//
+//	return 0;
+//}
