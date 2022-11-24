20a5bab8dd9b77e1a0884aa3fd9493238b016445
cha-yubai <2946053797@qq.com>
2022-11-24 19:28:03 +0800

    基础篇-循环语句

--- 基础/3、基础语法/3、基础语法/2、循环语句.c
+++ 基础/3、基础语法/3、基础语法/2、循环语句.c
@@ -1,19 +1,21 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
 //循环语句for/while/do-while 
-//#include <stdio.h>
-//int main()
-//{
-//	int line = 0;
-// 
-//	while (line < 30000)
-//	{//要循环的动作
-//		printf("继续写代码：%d\n",line);
-//		line++;
-//	}
-//	if (line >= 30000)
-//	{
-//		printf("恭喜，拿到好offer！");
-//	}
-//	return 0;
-//}
+/*
+#include <stdio.h>
+int main()
+{
+	int line = 0;
+ 
+	while (line < 30000)
+	{//要循环的动作
+		printf("继续写代码：%d\n",line);
+		line++;
+	}
+	if (line >= 30000)
+	{
+		printf("恭喜，拿到好offer！");
+	}
+	return 0;
+}
+*/
