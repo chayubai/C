184c493a07088216c043e3ed8eb8b431efc022eb
cha-yubai <2946053797@qq.com>
2022-11-24 19:27:48 +0800

    基础篇-选择语句

--- 基础/3、基础语法/3、基础语法/1、选择语句.c
+++ 基础/3、基础语法/3、基础语法/1、选择语句.c
@@ -3,20 +3,22 @@
 //选择语句if
 	//如果你好好学习，校招时拿好offer，走上人生巅峰
 	//如果你不学习，毕业等于失业，回家卖红薯
-//#include <stdio.h>
-//int main()
-//{
-//	int input = 0;
-//	printf("你会去敲代码吗？（选择1 or 0）:>");
-//	scanf("%d",&input);
-//	if (input == 1)
-//	{
-//		printf("坚持，你会有好offer！\n");
-//	}
-//	else
-//	{
-//		printf("放弃，回家卖红薯！\n");
-//	}
-//
-//	return 0;
-//}
+/*
+#include <stdio.h>
+int main()
+{
+	int input = 0;
+	printf("你会去敲代码吗？（选择1 or 0）:>");//底层write函数，向stdout写入数据
+	scanf("%d",&input);//底层read函数，向stdin读取数据
+	if (input == 1)
+	{
+		printf("坚持，你会有好offer！\n");
+	}
+	else
+	{
+		printf("放弃，回家卖红薯！\n");
+	}
+
+	return 0;
+}
+*/
