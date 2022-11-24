55e5226e82a647fce9f49e629d2f764562850f36
cha-yubai <2946053797@qq.com>
2022-11-24 19:28:17 +0800

    基础篇-函数

--- 基础/3、基础语法/3、基础语法/3、函数.c
+++ 基础/3、基础语法/3、基础语法/3、函数.c
@@ -3,36 +3,38 @@
 //函数
 //数学函数：f(x)=2x+1，即每一个x对应一个值
 //C语言的函数：Add(a,b)定义一个加法函数,每传一组a b,都有一组对应的输出结果
+/*
+#include <stdio.h>
+int main()
+{
+	int num1 = 0;
+	int num2 = 0;
+	printf("请输入两个整数：");
+	scanf("%d%d", &num1, &num2);
+	int sum = num1 + num2;
+	printf("sum = %d\n", sum);
 
-//#include <stdio.h>
-//int main()
-//{
-//	int num1 = 0;
-//	int num2 = 0;
-//	printf("请输入两个整数：");
-//	scanf("%d%d", &num1, &num2);
-//	int sum = num1 + num2;
-//	printf("sum = %d\n", sum);
-//
-//	return 0;
-//}
+	return 0;
+}
+*/
+/*
+#include <stdio.h>
+int Add(int x,int y)//函数的返回类型为int
+{//把两个数的相加抽象成一个方法，只要想要做加法，都可以使用这个方法，使用它的方法：函数名（参数1，参数2）
+	int z = 0;
+	z = x + y;
+	return z;//z是整型
+}
+int main()
+{
+	int num1 = 0;
+	int num2 = 0;
+	printf("请输入两个整数：");
+	scanf("%d%d", &num1, &num2);
+	int sum = Add(num1, num2);
+	printf("sum = %d\n", sum);
 
-//#include <stdio.h>
-//int Add(int x,int y)//函数的返回类型为int
-//{//把两个数的相加抽象成一个方法，只要想要做加法，都可以使用这个方法，使用它的方法：函数名（参数1，参数2）
-//	int z = 0;
-//	z = x + y;
-//	return z;//z是整型
-//}
-//int main()
-//{
-//	int num1 = 0;
-//	int num2 = 0;
-//	printf("请输入两个整数：");
-//	scanf("%d%d", &num1, &num2);
-//	int sum = Add(num1, num2);
-//	printf("sum = %d\n", sum);
-//
-//	return 0;
-//}
-////函数的特点：简化代码，代码复用
+	return 0;
+}
+*/
+//函数的特点：简化代码，代码复用
